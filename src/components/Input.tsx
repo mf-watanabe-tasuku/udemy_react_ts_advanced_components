d96a9eeb21d9dad5a904e0773ba715d8465cca1e
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-01-26 23:48:06 +0900

    Edit Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,13 @@
-export default function Input() {
-  return <input />;
+type InputProps = {
+  label: string;
+  id: string;
+}
+
+export default function Input({ label, id }: InputProps) {
+  return (
+    <p>
+      <label htmlFor={id}>{label}</label>
+      <input id={id} type="text" />
+    </p>
+  );
 }
