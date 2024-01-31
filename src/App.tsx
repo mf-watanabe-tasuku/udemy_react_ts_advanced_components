800a9752f9b3edba1d2f018b1e66410c289cab51
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-01-31 23:05:29 +0900

    Add custom form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { useRef } from 'react';
+import Form from './components/Form.tsx';
+import Button from './components/Button.tsx';
 import Input from './components/Input.tsx';
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
-
   return (
     <main>
-      <Input label="test" id="test" ref={input} />
+      <Form>
+        <Input type="text" label="Name" id="name" />
+        <Input type="number" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
