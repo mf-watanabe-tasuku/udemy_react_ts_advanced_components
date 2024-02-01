18f25f3d8d4bf00591d2fbaf192fdef279e5479f
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-01 22:54:09 +0900

    Add useImperativeHandle

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,19 +1,48 @@
-import { type FormEvent, type ComponentPropsWithoutRef } from "react";
+import {
+  type FormEvent,
+  type ComponentPropsWithoutRef,
+  forwardRef,
+  useRef,
+  useImperativeHandle
+} from "react";
+
+export type FormHandle = {
+  clear: () => void;
+}
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
   onSave: (value: unknown) => void;
 };
 
-export default function Form({ onSave, children, ...otherProps }: FormProps) {
+const Form = forwardRef<FormHandle, FormProps>(function Form({
+  onSave,
+  children,
+  ...otherProps
+}, ref) {
+  const form = useRef<HTMLFormElement>(null);
+
+  useImperativeHandle(ref, () => {
+    return {
+      clear() {
+        form.current?.reset();
+      }
+    }
+  });
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     const formData = new FormData(event.currentTarget);
     const data = Object.fromEntries(formData);
     onSave(data);
+    form.current?.reset();
   }
 
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>{children}</form>
+    <form onSubmit={handleSubmit} {...otherProps} ref={form}>
+      {children}
+    </form>
   )
-}
+});
+
+export default Form;
