b9c836c82fc832b372bb45b80f04746e561f471b
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-01 22:26:56 +0900

    Add unknown type

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import Button from './components/Button.tsx';
 import Input from './components/Input.tsx';
 
 function App() {
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+  }
+
   return (
     <main>
-      <Form>
-        <Input type="text" label="Name" id="name" />
-        <Input type="number" label="Age" id="age" />
+      <Form onSave={handleSave}>
+        <Input type="text" label="Name" id="name" name="name" />
+        <Input type="number" label="Age" id="age" name="age" />
         <p>
           <Button>Save</Button>
         </p>
