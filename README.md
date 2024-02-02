02d837b202ca22790233b2737e7957d82b1dfde2
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-02 23:41:14 +0900

    Restructure repository

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# React TypeScript Advanced Components
+# React TypeScript Practical Guide
 
-Udemyの動画内で作成した学習用アプリケーション
+Udemyの動画内で作成した学習用アプリケーション群
 
 https://www.udemy.com/course/react-typescript-the-practical-guide/
 
---
+++ react-ts-basics/src/components/InfoBox.tsx
@@ -0,0 +1,33 @@
+import { type PropsWithChildren } from "react";
+
+type HintBoxProps = PropsWithChildren<{
+  mode: 'hint';
+}>;
+
+type WarningBoxProps = PropsWithChildren<{
+  mode: 'warning';
+  severity: 'low' | 'medium' | 'high';
+}>;
+
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
+export default function InfoBox(props: InfoBoxProps) {
+  const { mode, children } = props;
+
+  if (mode === 'hint') {
+    return (
+      <aside className="infobox infobox-hint">
+        <p>{children}</p>
+      </aside>
+    );
+  }
+
+  const { severity } = props;
+
+  return (
+    <aside className={`infobox infobox-warning warning--${severity}`}>
+      {mode === 'warning' ? <h2>Warning</h2> : null}
+      <p>{children}</p>
+    </aside>
+  );
+}
