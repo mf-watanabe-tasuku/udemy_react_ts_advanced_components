4a6e24ab0a7e6ed9c57e9bac1145b132ec914c14
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-03 20:31:16 +0900

    Add timerReducer and define action types

--- react-ts-contextapi/src/components/AddTimer.tsx
+++ react-ts-contextapi/src/components/AddTimer.tsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import { useTimersContext } from '../store/timers-context.tsx';
 
 import Button from './UI/Button.tsx';
 import Form, { FormHandle } from './UI/Form.tsx';
@@ -6,10 +7,11 @@ import Input from './UI/Input.tsx';
 
 export default function AddTimer() {
   const form = useRef<FormHandle>(null);
+  const { addTimer } = useTimersContext();
 
   function handleSaveTimer(data: unknown) {
     const extractedData = data as { name: string; duration: string };
-    console.log(extractedData);
+    addTimer({ name: extractedData.name, duration: +extractedData.duration });
     form.current?.clear();
   }
 
