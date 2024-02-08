df5a51c254976047d915333a23ea7e281ed81a9c
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-09 00:27:39 +0900

    Handling Loading & Error States

--- data-fetching/src/App.tsx
+++ data-fetching/src/App.tsx
@@ -2,6 +2,7 @@ import { type ReactNode, useState, useEffect } from 'react';
 import BlogPosts, { type BlogPost } from './components/BlogPosts';
 import { get } from './util/http';
 import fetchingImg from './assets/data-fetching.png';
+import ErrorMessage from './components/ErrorMessage';
 
 type RawDataBlogPost = {
   id: number;
@@ -12,22 +13,32 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get(
-        'https://jsonplaceholder.typicode.com/posts'
-      )) as RawDataBlogPost[];
+      setIsFetching(true);
+      try {
+        const data = (await get(
+          'https://jsonplaceholder.typicode.com/posts'
+        )) as RawDataBlogPost[];
 
-      const blogPosts: BlogPost[] = data.map(rawPost => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body
+        const blogPosts: BlogPost[] = data.map(rawPost => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body
+          }
+        });
+        setFetchedPosts(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
         }
-      });
+      }
 
-      setFetchedPosts(blogPosts);
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -35,8 +46,16 @@ function App() {
 
   let content: ReactNode;
 
+  if (error) {
+    content = <ErrorMessage text={error} />
+  }
+
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Fetching posts...</p>;
   }
 
   return (
