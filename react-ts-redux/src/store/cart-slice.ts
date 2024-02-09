d080fd57d93191cd515c211a6dd37bc235f5c1e5
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-09 23:56:24 +0900

    Creating a Redux Store & A First Slice

---
+++ react-ts-redux/src/store/cart-slice.ts
@@ -0,0 +1,28 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+type CartItem = {
+  id: string;
+  title: string;
+  price: number;
+  quantity: number;
+};
+
+type CartState = {
+  items: CartItem[];
+};
+
+const initialState: CartState = {
+  items: []
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart(
+      state,
+      action: PayloadAction<{ id: string; title: string; price: number }>
+    ) {},
+    removeFromCart() {}
+  }
+});
