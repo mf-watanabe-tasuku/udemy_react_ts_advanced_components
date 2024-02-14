3dbfcb407e7c3737f3c4e94416e9eeb1dc5e377e
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-14 22:23:56 +0900

    Building a Wrapper Component That Renders Different Elements

--- advanced-components/src/App.tsx
+++ advanced-components/src/App.tsx
@@ -1,10 +1,14 @@
-import Input from './components/Input';
+import Button from "./components/Button";
 
 function App() {
   return (
     <main>
-      <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" />
+      <p>
+        <Button el="button">A Button</Button>
+      </p>
+      <p>
+        <Button el="anchor" href="https://google.com">A Link</Button>
+      </p>
     </main>
   );
 }
