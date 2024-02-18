286faeaee076ae784420e07ff7ab3869e595df01
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-18 18:49:32 +0900

    Sharing Login with "unknown" & Type Casting

--- advanced-components/src/App.tsx
+++ advanced-components/src/App.tsx
@@ -1,12 +1,22 @@
-import { useRef } from 'react';
 import Input from './components/Input';
+import Form from './components/Form';
+import Button from './components/Button';
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+  function handleSave(data: unknown) {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+  }
 
   return (
     <main>
-      <Input label="hoge" id="hoge" ref={input} />
+      <Form onSave={handleSave}>
+        <Input type="text" label="Name" id="name" />
+        <Input type="text" label="Age" id="age" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
     </main>
   );
 }
