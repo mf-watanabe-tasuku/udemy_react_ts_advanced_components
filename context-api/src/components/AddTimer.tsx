9dfae4e9a3683d2f9b097254b20f346a91269cc5
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-20 20:12:18 +0900

    Restart Context API project

--- context-api/src/components/AddTimer.tsx
+++ context-api/src/components/AddTimer.tsx
@@ -1,5 +1,4 @@
 import { useRef } from 'react';
-import { useTimersContext } from '../store/timers-context.tsx';
 
 import Button from './UI/Button.tsx';
 import Form, { FormHandle } from './UI/Form.tsx';
@@ -7,11 +6,10 @@ import Input from './UI/Input.tsx';
 
 export default function AddTimer() {
   const form = useRef<FormHandle>(null);
-  const { addTimer } = useTimersContext();
 
   function handleSaveTimer(data: unknown) {
     const extractedData = data as { name: string; duration: string };
-    addTimer({ name: extractedData.name, duration: +extractedData.duration });
+    console.log(extractedData);
     form.current?.clear();
   }
 
