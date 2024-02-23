04782173f3deb97beb196769c2eaa51299b4a446
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-23 15:47:35 +0900

    Wiring Everthing Up & Finishing the App

--- context-api/src/store/timers-context.tsx
+++ context-api/src/store/timers-context.tsx
@@ -1,6 +1,6 @@
 import { type ReactNode, createContext, useContext, useReducer } from 'react';
 
-type Timer = {
+export type Timer = {
   name: string;
   duration: number;
 }
@@ -37,20 +37,67 @@ type TimersContextProviderProps = {
   children: ReactNode;
 };
 
+type AddTimerACtion = {
+  type: 'ADD_TIMER';
+  payload: Timer;
+}
+
+type StartTimersACtion = {
+  type: 'START_TIMERS';
+}
+
+type StopTimersACtion = {
+  type: 'STOP_TIMERS';
+}
+
+type Action = AddTimerACtion | StartTimersACtion | StopTimersACtion;
+
+function timersReducer(state: TimersState, action: Action): TimersState {
+  switch (action.type) {
+    case 'START_TIMERS': {
+      return {
+        ...state,
+        isRunning: true
+      }
+    }
+    case 'STOP_TIMERS': {
+      return {
+        ...state,
+        isRunning: false
+      }
+    }
+    case 'ADD_TIMER': {
+      return {
+        ...state,
+        timers: [
+          ...state.timers,
+          {
+            name: action.payload.name,
+            duration: action.payload.duration
+          }
+        ]
+      }
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
 export default function TimersContextProvider({ children }: TimersContextProviderProps) {
-  useReducer(reducer, initialState);
+  const [timersState, dispatch] = useReducer(timersReducer, initialState);
 
   const ctx: TimersContextValue = {
-    timers: [],
-    isRunning: false,
+    timers: timersState.timers,
+    isRunning: timersState.isRunning,
     addTimer(timerData: Timer) {
-
+      dispatch({ type: 'ADD_TIMER', payload: timerData });
     },
     startTimers() {
-
+      dispatch({ type: 'START_TIMERS' });
     },
     stopTimers() {
-
+      dispatch({ type: 'STOP_TIMERS' });
     }
   }
 
