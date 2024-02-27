a2ff9d0d3211284ae3eba0e3eb5e7352d2fa6031
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-02-28 08:26:19 +0900

    Using the zod Library for Response Data Validation

--- data-fetching/src/App.tsx
+++ data-fetching/src/App.tsx
@@ -1,4 +1,26 @@
+import { useEffect } from 'react';
+import { z } from 'zod';
+import { get } from './util/http';
+
 function App() {
+  useEffect(() => {
+    const rawDataBlogPostSchema = z.object({
+      id: z.number(),
+      userId: z.number(),
+      title: z.string(),
+      body: z.string()
+    });
+
+    const fetchData = async () => {
+      const data = await get(
+        'https://jsonplaceholder.typicode.com/posts',
+        z.array(rawDataBlogPostSchema)
+      );
+      return data;
+    }
+    fetchData();
+  })
+
   return <h1>Data Fetching!</h1>;
 }
 
