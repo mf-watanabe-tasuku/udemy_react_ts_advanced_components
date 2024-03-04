0738ba6e0a64d0778d40f1caf0f69e3a51baaf3f
Watanabe Tasuku <watanabe.tasuku@moneyforward.co.jp>
2024-03-04 21:32:32 +0900

    Selecting & Transforming Redux Store Data

--- redux/src/store/hooks.ts
+++ redux/src/store/hooks.ts
@@ -4,5 +4,4 @@ import { type AppDispatch, type RootState } from './store';
 type DispatchFunction = () => AppDispatch;
 
 export const useCartDispatch: DispatchFunction = useDispatch;
-
 export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
