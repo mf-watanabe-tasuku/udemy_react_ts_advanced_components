7c3411637639ab1807f2f287d6a4b460dc201ee4
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-03-12 00:30:43 +0900

    Update Sessions

--- practice-project/src/pages/Sessions.tsx
+++ practice-project/src/pages/Sessions.tsx
@@ -1,37 +1,43 @@
 import { SESSIONS } from '../dummy-sessions.ts'; // normally, we would probably load that from a server
+import MainHeader from '../components/Navigation/MainHeader';
 import Button from '../components/UI/Button.tsx';
 
 export default function SessionsPage() {
   const camelToDash = (str: string) => str.replace(/[A-Z]/g, letter => `-${letter.toLowerCase()}`);
 
   return (
-    <main id="sessions-page">
-      <header>
-        <h2>Available mentoring sessions</h2>
-        <p>
-          From an one-on-one introduction to React's basics all the way up to a
-          deep dive into state mechanics - we got just the right session for
-          you!
-        </p>
-      </header>
-      <ul id="sessions-list">
-        {SESSIONS.map(session => {
-          return (
-            <li>
-              <article className='session-item'>
-                <img src={camelToDash(session.image)} alt={session.title} />
-                <div className="session-data">
-                  <div></div>
-                  <p className="actions">
-                    <Button to={session.id}>Learn More</Button>
-                  </p>
-                </div>
-              </article>
-            </li>
-          );
-        })};
-      </ul>
-
-    </main>
+    <>
+      <MainHeader />
+      <main id="sessions-page">
+        <header>
+          <h2>Available mentoring sessions</h2>
+          <p>
+            From an one-on-one introduction to React's basics all the way up to a
+            deep dive into state mechanics - we got just the right session for
+            you!
+          </p>
+        </header>
+        <ul id="sessions-list">
+          {SESSIONS.map(session => {
+            return (
+              <li key={session.id}>
+                <article className='session-item'>
+                  <img src={camelToDash(session.image)} alt={session.title} />
+                  <div className="session-data">
+                    <div>
+                      <h3>{session.title}</h3>
+                      <p>{session.summary}</p>
+                    </div>
+                    <p className="actions">
+                      <Button to={session.id}>Learn More</Button>
+                    </p>
+                  </div>
+                </article>
+              </li>
+            );
+          })};
+        </ul>
+      </main>
+    </>
   );
 }
