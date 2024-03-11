ae0f65ee53b1e0e53fa78dab8b8268670fcee31f
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-03-12 00:30:30 +0900

    Update MainHeader

--- practice-project/src/components/Navigation/MainHeader.tsx
+++ practice-project/src/components/Navigation/MainHeader.tsx
@@ -1,13 +1,15 @@
 import Button from "../UI/Button";
 
 const MainHeader = () => {
+  const isCurrentPage = (path: string) => path === location.pathname ? true : false;
+
   return (
     <header id="main-header">
       <h1>ReactMentoring</h1>
       <nav>
         <ul>
-          <li><a href="/" className="active">Our Mission</a></li>
-          <li><a href="/sessions" className="">Browse Sessions</a></li>
+          <li><a href="/" className={isCurrentPage('/') ? 'active' : ''}>Our Mission</a></li>
+          <li><a href="/sessions" className={isCurrentPage('/sessions') ? 'active' : ''}>Browse Sessions</a></li>
           <li><Button>Upcoming Sessions</Button></li>
         </ul>
       </nav>
