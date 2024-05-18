46b1098d68dbe936aad68bc8d7b1992343d23b82
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-05-18 12:43:56 +0900

    Building components with discriminated unions

--- section4/src/components/CourseGoalList.tsx
+++ section4/src/components/CourseGoalList.tsx
@@ -14,7 +14,7 @@ export default function CourseGoalList({
 }: CourseGoalListProps) {
   if (goals.length === 0) {
     return (
-      <InfoBox mode='hint' severity="low">
+      <InfoBox mode='hint'>
         You have no course goals yet. Start adding some!
       </InfoBox>
     );
@@ -24,7 +24,7 @@ export default function CourseGoalList({
 
   if (goals.length > 4) {
     warningBox = (
-      <InfoBox mode='warning' severity="high">
+      <InfoBox mode='warning' severity="medium">
         You're collecting a lot of goals. Don't put too much on your plate!
       </InfoBox>
     );
