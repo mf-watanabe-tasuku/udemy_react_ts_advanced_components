302334473394189b199ff097924ddb703f6a3593
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-05-19 00:49:04 +0900

    Working with type predicates & facing typescript limitations

--- 2024-May/section4-2/src/App.tsx
+++ 2024-May/section4-2/src/App.tsx
@@ -1,17 +1,14 @@
 import Button from './components/Button';
-import Input from './components/Input';
 
 function App() {
   return (
     <main>
       <p>
-        <Button el="button">A Button</Button>
+        <Button>A Button</Button>
       </p>
       <p>
-        <Button el="anchor" href="https://google.com">A Link</Button>
+        <Button href="https://google.com">A Link</Button>
       </p>
-      {/* <Input id="name" label="Your name" type="text" />
-      <Input id="age" label="Your age" type="number" /> */}
     </main>
   );
 }
