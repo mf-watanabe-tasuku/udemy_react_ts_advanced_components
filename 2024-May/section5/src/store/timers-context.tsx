8152b18f2a51f19edab3e4fd47bc49b76e5d4231
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-05-21 07:20:48 +0900

    Using better action types

--- 2024-May/section5/src/store/timers-context.tsx
+++ 2024-May/section5/src/store/timers-context.tsx
@@ -37,23 +37,22 @@ type TimersContextProviderProps = {
   children: ReactNode;
 };
 
-type Action = {
-  type: 'ADD_TIMER' | 'STOP_TIMERS' | 'START_TIMERS';
+type StartTimersAction = {
+  type: 'START_TIMERS';
 }
 
+type StopTimersAction = {
+  type: 'STOP_TIMERS';
+}
+
+type AddTimerAction = {
+  type: 'ADD_TIMER';
+  payload: Timer;
+}
+
+type Action = StartTimersAction | StopTimersAction | AddTimerAction;
+
 function timersReducer(state: TimersState, action: Action): TimersState {
-  if (action.type === 'ADD_TIMER') {
-    return {
-      ...state,
-      timers: [
-        ...state.timers,
-        {
-          name,
-          duration,
-        }
-      ],
-    }
-  }
   if (action.type === 'START_TIMERS') {
     return {
       ...state,
@@ -66,19 +65,39 @@ function timersReducer(state: TimersState, action: Action): TimersState {
       isRunning: false,
     }
   }
+  if (action.type === 'ADD_TIMER') {
+    return {
+      ...state,
+      timers: [
+        ...state.timers,
+        {
+          name: action.payload.name,
+          duration: action.payload.duration,
+        }
+      ],
+    }
+  }
+
+  return state;
 }
 
 export default function TimersContextProvider({
   children,
 }: TimersContextProviderProps) {
   const [timersState, dispatch] = useReducer(timersReducer, initialState);
-  
+
   const ctx: TimersContextValue = {
-    timers: [],
-    isRunning: false,
-    addTimer(timerData) {},
-    startTimers() {},
-    stopTimers() {},
+    timers: timersState.timers,
+    isRunning: timersState.isRunning,
+    addTimer(timerData) {
+      dispatch({ type: 'ADD_TIMER', payload: timerData})
+    },
+    startTimers() {
+      dispatch({ type: 'START_TIMERS' })
+    },
+    stopTimers() {
+      dispatch({ type: 'STOP_TIMERS' })
+    },
   };
 
   return (
